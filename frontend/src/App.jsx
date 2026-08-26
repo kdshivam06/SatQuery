01e1eb269260d64a9920edf0e3ffec0569a92e6f
Devarshidesale <devarshibdesale15@gmail.com>
2026-08-27 01:33:54 +0530

    preloader screen and landing page designing completed, file uploading and query input field implementation.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Preloader from './pages/Preloader';
+import './styles/preloader.css';
+import './styles/hero.css';
+import './App.css';
+
+function App() {
+  return (
+    <main className="app-container">
+      <Preloader />
+    </main>
+  );
+}
+
+export default App;
